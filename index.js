6881866717ed1411b67abaefe2156b62482c1093
Juan Salazar <ferthelet@gmail.com>
2023-03-07 21:32:43 -0600

    adds countdown, including 0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // js
 
 function writeCards(name, event) {
-  //returns an array of thank you messages for each name provided to the function
+  // returns an array of thank you messages for each name provided to the function
   const newMessages = []; // was not working, declaration was with {}
   for (let i = 0; i < name.length; i++) {
     newMessages[i] = `Thank you, ${name[i]}, for the wonderful ${event} gift!`;
@@ -12,4 +12,12 @@ function writeCards(name, event) {
 
 const messages = writeCards(["Charlie", "Samip", "Ali"], "birthday");
 
+function countDown(positiveNum) {
+  // invokes console.log once for each number, counting down from the number provided to zero
+  let i = positiveNum;
 
+  while (i >= 0) {
+    console.log(i);
+    i--;
+  }
+}
