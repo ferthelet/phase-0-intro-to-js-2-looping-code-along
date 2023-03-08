999ca763022f5528196b2b5a39104e56fe9987b6
Juan Salazar <ferthelet@gmail.com>
2023-03-07 21:25:08 -0600

    adds thank you notes

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+// js
+
+function writeCards(name, event) {
+  //returns an array of thank you messages for each name provided to the function
+  const newMessages = []; // was not working, declaration was with {}
+  for (let i = 0; i < name.length; i++) {
+    newMessages[i] = `Thank you, ${name[i]}, for the wonderful ${event} gift!`;
+  }
+  return newMessages;
+}
+
+const messages = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+
